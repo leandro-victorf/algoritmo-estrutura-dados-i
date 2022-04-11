200d11c36fb9b9de33ad19126805329c30f4fd96
Leandro Vicotr <leandro.victorf@gmail.com>
2022-04-10 21:33:14 -0300

    Fiz line

--- atividade1/bissexto.c
+++ atividade1/bissexto.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
-int main() {
-  
+int main()
+{
+
   int ano;
   scanf("%d", &ano);
 
-  while(ano < 1582 || ano >1000000){
+  while (ano < 1582 || ano > 1000000)
+  {
     scanf("%d", &ano);
   }
-  float n = ((ano - 1580.0)/4) + 1;
-  if( n == (int)n){
-    float razao = ano/100.0;
-    float razao_dois= ano/400.0;
-    if (razao == (int)razao & razao_dois != (int)razao_dois) {
+  float n = ((ano - 1580.0) / 4) + 1;
+  if (n == (int)n)
+  {
+    float razao = ano / 100.0;
+    float razao_dois = ano / 400.0;
+    if (razao == (int)razao & razao_dois != (int)razao_dois)
+    {
       printf("ANO NAO BISSEXTO\n");
-    }else{
+    }
+    else
+    {
       printf("ANO BISSEXTO\n");
     }
-  }else{
+  }
+  else
+  {
     printf("ANO NAO BISSEXTO\n");
   }
 }
-  
--- atividade1/buscaPalavra.c
+++ atividade1/buscaPalavra.c
@@ -4,8 +4,8 @@
 int main() {
   int n, q;
   scanf("%d", &n);
-  char palavras[25][25];
-  char tentativas[25][25];
+  char palavras[n][25];
+  char tentativa[25];
   int contador = 0;
   
   for (int i = 0; i < n; i++){
@@ -13,17 +13,15 @@ int main() {
   }
   scanf("%d", &q);
   for (int i = 0; i < q; i++){
-    scanf("%s", tentativas[i]);
-  }
-
-  for (int i = 0; i < q; i++){
+  	scanf("%s", tentativa);
     for (int j = 0; j < n; j++){
-      if (strcmp(tentativas[i], palavras[j]) == 0){
-        contador++;
+      if(strcmp(tentativa, palavras[j]) == 0){
+        contador ++;
       }
     }
     printf("%i\n", contador);
     contador = 0;
   }
+  return 0;
 }
   
