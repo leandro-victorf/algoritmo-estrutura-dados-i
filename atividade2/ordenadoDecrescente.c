e293e58652531b8d9e562de86159ce825b8bcc17
Leandro Vicotr <leandro.victorf@gmail.com>
2022-04-10 21:39:57 -0300

    Add ordenadoDecrescente

---
+++ atividade2/ordenadoDecrescente.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, contador = 0;
+    int vetor[n];
+    scanf("%d", &n);
+
+    if (n == 0)
+    {
+        printf("S\n");
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &vetor[i]);
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            if (vetor[i - 1] < vetor[i])
+            {
+                contador++;
+            }
+        }
+
+        if (contador == 0)
+        {
+            printf("S\n");
+        }
+        else
+        {
+            printf("N\n");
+        }
+    }
+}
